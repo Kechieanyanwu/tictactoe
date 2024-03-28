4aba1346a41e945df181cc1fb7114cb1b877b10d
Nkechi Anyanwu <kechie.94@gmail.com>
2024-03-28 18:45:39 +0000

    Included Ascending and Descending toggle

--- src/components/Board.js
+++ src/components/Board.js
@@ -52,29 +52,20 @@ function Board({xIsNext, squares, onPlay}) {
             {renderBoard()}
         </>
     )
-    
+}
 
-    // return (
-    // <>
-    //     <div className="status">{status}</div>
-    //     <div className="board-row">
-    //         <Square value={squares[0]} onSquareClick={ () => handleClick(0)} />   
-    //         <Square value={squares[1]} onSquareClick={ () => handleClick(1)} />   
-    //         <Square value={squares[2]} onSquareClick={ () => handleClick(2)} />   
-    //     </div>
-    //     <div className="board-row">
-    //         <Square value={squares[3]} onSquareClick={ () => handleClick(3)} />   
-    //         <Square value={squares[4]} onSquareClick={ () => handleClick(4)} />   
-    //         <Square value={squares[5]} onSquareClick={ () => handleClick(5)} />   
-    //     </div>
-    //     <div className="board-row">
-    //         <Square value={squares[6]} onSquareClick={ () => handleClick(6)} />   
-    //         <Square value={squares[7]} onSquareClick={ () => handleClick(7)} />   
-    //         <Square value={squares[8]} onSquareClick={ () => handleClick(8)} />   
-    //     </div>
+function Toggle({isAsc, onToggleClick}) {
+    const [asc, setAsc] = useState(isAsc);
 
-    // </>
-    // )
+    function handleClick() {
+        onToggleClick();
+        setAsc(!asc);
+    }
+
+    const order = isAsc ? "Ascending": "Descending";
+
+    return <button onClick={handleClick}>{order} order</button>
+
 }
 
 export default function Game() {
@@ -82,6 +73,7 @@ export default function Game() {
     const [currentMove, setCurrentMove] = useState(0);
     const xIsNext = currentMove % 2 === 0;
     const currentSquares = history[currentMove];
+    const [isAsc, setIsAsc] = useState(true);
 
     function handlePlay(nextSquares) {
         const nextHistory = [...history.slice(0, currentMove + 1), nextSquares]
@@ -91,6 +83,10 @@ export default function Game() {
 
     function jumpTo(nextMove) {
         setCurrentMove(nextMove);
+    }
+
+    function handleToggle() {
+        setIsAsc(!isAsc);
     }
 
     const moves = history.map((squares, move) => {
@@ -114,13 +110,16 @@ export default function Game() {
         )
     })
 
+    const reversedMoves = [...moves].reverse();
+
     return (
         <div className="game">
             <div className="game-board">
                 <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
             </div>
             <div className="game-info">
-                <ol>{moves}</ol>
+                <Toggle isAsc={isAsc} onToggleClick={() => handleToggle()} />
+                <ol>{isAsc ? moves : reversedMoves}</ol>
             </div>
         </div>
     );
